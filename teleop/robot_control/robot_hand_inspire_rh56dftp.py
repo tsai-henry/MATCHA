86bd294dc7c0f9f05cfe8efb9fd9fe469e5fce8b
Anirudh Pai <anirudh.n.pai@gmail.com>
2025-04-28 23:59:10 +0000

    cleanup, untested

--- teleop/robot_control/robot_hand_inspire_rh56dftp.py
+++ teleop/robot_control/robot_hand_inspire_rh56dftp.py
@@ -1,81 +1,64 @@
 import time
 import numpy as np
 import threading
-from multiprocessing import Process, Array, Lock
-from unitree_sdk2py.core.channel import ChannelPublisher, ChannelSubscriber, ChannelFactoryInitialize
+from multiprocessing import Process, Array
+from unitree_sdk2py.core.channel import ChannelPublisher, ChannelSubscriber
 from inspire_sdkpy import inspire_dds, inspire_hand_defaut
 from teleop.robot_control.hand_retargeting import HandRetargeting, HandType
 
-# Constants
-Inspire_Num_Motors = 6
+inspire_num_motors = 6
 inspire_tip_indices = [4, 9, 14, 19, 24]
-kTopicInspireCommandR = "rt/inspire_hand/ctrl/r"
-kTopicInspireCommandL = "rt/inspire_hand/ctrl/l"
-kTopicInspireStateR = "rt/inspire_hand/state/r"
-kTopicInspireStateL = "rt/inspire_hand/state/l"
+command_topic_r = "rt/inspire_hand/ctrl/r"
+command_topic_l = "rt/inspire_hand/ctrl/l"
+state_topic_r = "rt/inspire_hand/state/r"
+state_topic_l = "rt/inspire_hand/state/l"
 
-class InspireControllerNewGen:
+class InspireControllerRH56DFTP:
     def __init__(self, left_hand_array, right_hand_array, dual_hand_data_lock=None, dual_hand_state_array=None, dual_hand_action_array=None, fps=100.0, Unit_Test=False):
         print("Initializing Inspire_Controller...")
         self.fps = fps
         self.Unit_Test = Unit_Test
 
-        # if not self.Unit_Test:
-        #     print('here')
-        #     ChannelFactoryInitialize(0, None)  # Change if needed
-        # else:
-        #     print('here2')
-        #     ChannelFactoryInitialize(0)
-
-        # Initialize publishers
-        self.left_hand_pub = ChannelPublisher(kTopicInspireCommandL, inspire_dds.inspire_hand_ctrl)
-        self.right_hand_pub = ChannelPublisher(kTopicInspireCommandR, inspire_dds.inspire_hand_ctrl)
+        self.left_hand_pub = ChannelPublisher(command_topic_l, inspire_dds.inspire_hand_ctrl)
+        self.right_hand_pub = ChannelPublisher(command_topic_r, inspire_dds.inspire_hand_ctrl)
         self.left_hand_pub.Init()
         self.right_hand_pub.Init()
 
-        # Initialize subscribers
-        self.left_hand_sub = ChannelSubscriber(kTopicInspireStateL, inspire_dds.inspire_hand_state)
-        self.right_hand_sub = ChannelSubscriber(kTopicInspireStateR, inspire_dds.inspire_hand_state)
+        self.left_hand_sub = ChannelSubscriber(state_topic_l, inspire_dds.inspire_hand_state)
+        self.right_hand_sub = ChannelSubscriber(state_topic_r, inspire_dds.inspire_hand_state)
         self.left_hand_sub.Init()
         self.right_hand_sub.Init()
 
-        # Shared Arrays
-        self.left_hand_state_array = Array('d', Inspire_Num_Motors, lock=True)
-        self.right_hand_state_array = Array('d', Inspire_Num_Motors, lock=True)
+        self.left_hand_state_array = Array('d', inspire_num_motors, lock=True)
+        self.right_hand_state_array = Array('d', inspire_num_motors, lock=True)
 
-        # External Shared Arrays
         self.shared_left_hand_array = left_hand_array
         self.shared_right_hand_array = right_hand_array
         self.dual_hand_data_lock = dual_hand_data_lock
         self.dual_hand_state_array = dual_hand_state_array
         self.dual_hand_action_array = dual_hand_action_array
 
-        # Initialize hand retargeting
         if not self.Unit_Test:
             self.hand_retargeting = HandRetargeting(HandType.INSPIRE_HAND)
         else:
             self.hand_retargeting = HandRetargeting(HandType.INSPIRE_HAND_Unit_Test)
 
-        # Control message
         self.left_cmd = inspire_hand_defaut.get_inspire_hand_ctrl()
         self.right_cmd = inspire_hand_defaut.get_inspire_hand_ctrl()
         self.left_cmd.mode = 0b0001
         self.right_cmd.mode = 0b0001
 
-        # Start subscription thread
         self.running = True
         self.subscribe_state_thread = threading.Thread(target=self._subscribe_hand_state)
         self.subscribe_state_thread.daemon = True
         self.subscribe_state_thread.start()
 
-        # Wait until some data received
         while True:
             if any(self.right_hand_state_array):
                 break
             print("[Inspire_Controller] Waiting to subscribe DDS...")
             time.sleep(0.01)
 
-        # Start control process
         self.hand_control_process = Process(
             target=self.control_process,
             args=(self.shared_left_hand_array, self.shared_right_hand_array, self.left_hand_state_array, self.right_hand_state_array, self.dual_hand_data_lock, self.dual_hand_state_array, self.dual_hand_action_array)
@@ -89,12 +72,12 @@ class InspireControllerNewGen:
         while self.running:
             left_msg = self.left_hand_sub.Read()
             if left_msg is not None:
-                for idx in range(Inspire_Num_Motors):
+                for idx in range(inspire_num_motors):
                     self.left_hand_state_array[idx] = left_msg.angle_act[idx] / 1000.0  # millidegree → degree
 
             right_msg = self.right_hand_sub.Read()
             if right_msg is not None:
-                for idx in range(Inspire_Num_Motors):
+                for idx in range(inspire_num_motors):
                     self.right_hand_state_array[idx] = right_msg.angle_act[idx] / 1000.0
 
             time.sleep(0.002)
@@ -114,15 +97,12 @@ class InspireControllerNewGen:
             while self.running:
                 start_time = time.time()
 
-                # Read input hand tracking
                 left_hand_mat = np.array(left_hand_array[:]).reshape(25, 3).copy()
                 right_hand_mat = np.array(right_hand_array[:]).reshape(25, 3).copy()
 
-                # Default q_target
-                left_q_target = np.ones(Inspire_Num_Motors)
-                right_q_target = np.ones(Inspire_Num_Motors)
+                left_q_target = np.ones(inspire_num_motors)
+                right_q_target = np.ones(inspire_num_motors)
 
-                # Check if valid hand data
                 if not np.all(right_hand_mat == 0.0) and not np.all(left_hand_mat[inspire_tip_indices[0]] == np.array([-1.13, 0.3, 0.15])):
                     ref_left_value = left_hand_mat[inspire_tip_indices]
                     ref_right_value = right_hand_mat[inspire_tip_indices]
@@ -134,7 +114,7 @@ class InspireControllerNewGen:
                     def normalize(val, min_val, max_val):
                         return np.clip((max_val - val) / (max_val - min_val), 0.0, 1.0)
 
-                    for idx in range(Inspire_Num_Motors):
+                    for idx in range(inspire_num_motors):
                         if idx <= 3:
                             left_q_target[idx] = normalize(left_q_target[idx], 0.0, 1.7)
                             right_q_target[idx] = normalize(right_q_target[idx], 0.0, 1.7)
@@ -145,7 +125,6 @@ class InspireControllerNewGen:
                             left_q_target[idx] = normalize(left_q_target[idx], -0.1, 1.3)
                             right_q_target[idx] = normalize(right_q_target[idx], -0.1, 1.3)
 
-                # Update shared memory
                 if dual_hand_state_array and dual_hand_action_array:
                     with dual_hand_data_lock:
                         state_data = np.concatenate((np.array(left_hand_state_array[:]), np.array(right_hand_state_array[:])))
@@ -153,11 +132,10 @@ class InspireControllerNewGen:
                         dual_hand_state_array[:] = state_data
                         dual_hand_action_array[:] = action_data
 
-                # Publish control
                 self.ctrl_dual_hand(left_q_target, right_q_target)
 
                 elapsed = time.time() - start_time
                 sleep_time = max(0, (1.0 / self.fps) - elapsed)
                 time.sleep(sleep_time)
         finally:
-            print("Inspire_Controller control_process ended.")
+            print("InspireControllerRH56DFTP control_process ended.")
--- teleop/teleop_hand_and_arm.py
+++ teleop/teleop_hand_and_arm.py
@@ -12,11 +12,10 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
 from teleop.open_television.tv_wrapper import TeleVisionWrapper
-from teleop.robot_control.robot_arm import G1_29_ArmController, G1_23_ArmController, H1_2_ArmController, H1_ArmController
-from teleop.robot_control.robot_arm_ik import G1_29_ArmIK, G1_23_ArmIK, H1_2_ArmIK, H1_ArmIK
-# from teleop.robot_control.robot_hand_unitree import Dex3_1_Controller, Gripper_Controller
-# from teleop.robot_control.robot_hand_inspire import Inspire_Controller
-from teleop.robot_control.robot_hand_inspire_newgen import InspireControllerNewGen
+
+from teleop.robot_control.robot_arm import H1_2_ArmController
+from teleop.robot_control.robot_arm_ik import H1_2_ArmIK
+from teleop.robot_control.robot_hand_inspire_rh56dftp import InspireControllerRH56DFTP as InspireController
 from teleop.image_server.image_client import ImageClient
 from teleop.utils.episode_writer import EpisodeWriter
 
@@ -30,8 +29,8 @@ if __name__ == '__main__':
     parser.add_argument('--no-record', dest = 'record', action = 'store_false', help = 'Do not save data')
     parser.set_defaults(record = False)
 
-    parser.add_argument('--arm', type=str, choices=['G1_29', 'G1_23', 'H1_2', 'H1'], default='G1_29', help='Select arm controller')
-    parser.add_argument('--hand', type=str, choices=['inspire1_newgen'], help='Select hand controller')
+    parser.add_argument('--arm', type=str, choices=['H1_2'], default='H1_2', help='Select arm controller')
+    parser.add_argument('--hand', type=str, choices=['inspire'], default='inspire', help='Select hand controller')
 
     args = parser.parse_args()
     print(f"args:{args}\n")
@@ -78,50 +77,21 @@ if __name__ == '__main__':
     tv_wrapper = TeleVisionWrapper(BINOCULAR, tv_img_shape, tv_img_shm.name)
 
     # arm
-    if args.arm == 'G1_29':
-        arm_ctrl = G1_29_ArmController()
-        arm_ik = G1_29_ArmIK()
-    elif args.arm == 'G1_23':
-        arm_ctrl = G1_23_ArmController()
-        arm_ik = G1_23_ArmIK()
-    elif args.arm == 'H1_2':
+    if args.arm == 'H1_2':
         arm_ctrl = H1_2_ArmController()
         arm_ik = H1_2_ArmIK()
-    elif args.arm == 'H1':
-        arm_ctrl = H1_ArmController()
-        arm_ik = H1_ArmIK()
+    else:
+        raise ValueError(f"Unsupported arm controller: {args.arm}")
 
-    # have to comment out because of dependency conflict with inspire_sdk (used for new hands)
-    # if args.hand == "dex3":
-    #     left_hand_array = Array('d', 75, lock = True)         # [input]
-    #     right_hand_array = Array('d', 75, lock = True)        # [input]
-    #     dual_hand_data_lock = Lock()
-    #     dual_hand_state_array = Array('d', 14, lock = False)  # [output] current left, right hand state(14) data.
-    #     dual_hand_action_array = Array('d', 14, lock = False) # [output] current left, right hand action(14) data.
-    #     hand_ctrl = Dex3_1_Controller(left_hand_array, right_hand_array, dual_hand_data_lock, dual_hand_state_array, dual_hand_action_array)
-    # elif args.hand == "gripper":
-    #     left_hand_array = Array('d', 75, lock=True)
-    #     right_hand_array = Array('d', 75, lock=True)
-    #     dual_gripper_data_lock = Lock()
-    #     dual_gripper_state_array = Array('d', 2, lock=False)   # current left, right gripper state(2) data.
-    #     dual_gripper_action_array = Array('d', 2, lock=False)  # current left, right gripper action(2) data.
-    #     gripper_ctrl = Gripper_Controller(left_hand_array, right_hand_array, dual_gripper_data_lock, dual_gripper_state_array, dual_gripper_action_array)
-    # elif args.hand == "inspire1":
-    #     left_hand_array = Array('d', 75, lock = True)          # [input]
-    #     right_hand_array = Array('d', 75, lock = True)         # [input]
-    #     dual_hand_data_lock = Lock()
-    #     dual_hand_state_array = Array('d', 12, lock = False)   # [output] current left, right hand state(12) data.
-    #     dual_hand_action_array = Array('d', 12, lock = False)  # [output] current left, right hand action(12) data.
-    #     hand_ctrl = Inspire_Controller(left_hand_array, right_hand_array, dual_hand_data_lock, dual_hand_state_array, dual_hand_action_array)
-    if args.hand == "inspire1_newgen":
+    if args.hand == "inspire":
         left_hand_array = Array('d', 75, lock = True)          # [input]
         right_hand_array = Array('d', 75, lock = True)         # [input]
         dual_hand_data_lock = Lock()
         dual_hand_state_array = Array('d', 12, lock = False)   # [output] current left, right hand state(12) data.
         dual_hand_action_array = Array('d', 12, lock = False)  # [output] current left, right hand action(12) data.
-        hand_ctrl = InspireControllerNewGen(left_hand_array, right_hand_array, dual_hand_data_lock, dual_hand_state_array, dual_hand_action_array)
+        hand_ctrl = InspireController(left_hand_array, right_hand_array, dual_hand_data_lock, dual_hand_state_array, dual_hand_action_array)
     else:
-        pass
+        raise ValueError(f"Unsupported hand controller: {args.hand}")
     
     if args.record:
         recorder = EpisodeWriter(task_dir = args.task_dir, frequency = args.frequency, rerun_log = True)
@@ -168,20 +138,7 @@ if __name__ == '__main__':
 
                 # record data
                 if args.record:
-                    # dex hand or gripper
-                    # if args.hand == "dex3":
-                    #     with dual_hand_data_lock:
-                    #         left_hand_state = dual_hand_state_array[:7]
-                    #         right_hand_state = dual_hand_state_array[-7:]
-                    #         left_hand_action = dual_hand_action_array[:7]
-                    #         right_hand_action = dual_hand_action_array[-7:]
-                    # elif args.hand == "gripper":
-                    #     with dual_gripper_data_lock:
-                    #         left_hand_state = [dual_gripper_state_array[1]]
-                    #         right_hand_state = [dual_gripper_state_array[0]]
-                    #         left_hand_action = [dual_gripper_action_array[1]]
-                    #         right_hand_action = [dual_gripper_action_array[0]]
-                    if args.hand == "inspire1" or args.hand == "inspire1_newgen":
+                    if args.hand == "inspire":
                         left_hand_state = dual_hand_state_array[:6]
                         right_hand_state = dual_hand_state_array[-6:]
                         left_hand_action = dual_hand_action_array[:6]
@@ -190,9 +147,11 @@ if __name__ == '__main__':
                         pass
                     # head image
                     current_tv_image = tv_img_array.copy()
+
                     # wrist image
                     if WRIST:
                         current_wrist_image = wrist_img_array.copy()
+
                     # arm state and action
                     left_arm_state  = current_lr_arm_q[:7]
                     right_arm_state = current_lr_arm_q[-7:]
@@ -218,51 +177,35 @@ if __name__ == '__main__':
                                 colors[f"color_{2}"] = current_wrist_image[:, wrist_img_shape[1]//2:]
                         states = {
                             "left_arm": {                                                                    
-                                "qpos":   left_arm_state.tolist(),    # numpy.array -> list
-                                "qvel":   [],                          
-                                "torque": [],                        
+                                "dof_angles":   left_arm_state.tolist(),              
                             }, 
                             "right_arm": {                                                                    
-                                "qpos":   right_arm_state.tolist(),       
-                                "qvel":   [],                          
-                                "torque": [],                         
+                                "dof_angles":   right_arm_state.tolist(),                            
                             },                        
                             "left_hand": {                                                                    
-                                "qpos":   left_hand_state,           
-                                "qvel":   [],                           
-                                "torque": [],                          
+                                "dof_angles":   left_hand_state,                                 
                             }, 
                             "right_hand": {                                                                    
-                                "qpos":   right_hand_state,       
-                                "qvel":   [],                           
-                                "torque": [],  
+                                "dof_angles":   right_hand_state,
                             }, 
-                            "body": None, 
                         }
                         actions = {
                             "left_arm": {                                   
-                                "qpos":   left_arm_action.tolist(),       
-                                "qvel":   [],       
-                                "torque": left_arm_torque.tolist(),      
+                                "dof_angles":   left_arm_action.tolist(),       
+                                "dof_torques": left_arm_torque.tolist(),      
                             }, 
                             "right_arm": {                                   
-                                "qpos":   right_arm_action.tolist(),       
-                                "qvel":   [],       
-                                "torque": right_arm_torque.tolist(),       
+                                "dof_angles":   right_arm_action.tolist(),       
+                                "dof_torques": right_arm_torque.tolist(),       
                             },                         
                             "left_hand": {                                   
-                                "qpos":   left_hand_action,       
-                                "qvel":   [],       
-                                "torque": [],       
+                                "dof_angles":   left_hand_action,   
                             }, 
                             "right_hand": {                                   
-                                "qpos":   right_hand_action,       
-                                "qvel":   [],       
-                                "torque": [], 
+                                "dof_angles":   right_hand_action,
                             }, 
-                            "body": None, 
                         }
-                        recorder.add_item(colors=colors, depths=depths, states=states, actions=actions)
+                        recorder.add_item(colors=colors, states=states, actions=actions)
 
                 current_time = time.time()
                 time_elapsed = current_time - start_time
