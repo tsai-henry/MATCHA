9ecc32e19e8beb9e096c756832ebbc6cee9b1705
SDH4 Razer Laptop <anirudh.n.pai@gmail.com>
2025-04-27 22:32:45 -0700

    new inspire controller implemented, working with our hands. need to cleanup. data collection for hands also working

--- teleop/teleop_hand_and_arm.py
+++ teleop/teleop_hand_and_arm.py
@@ -14,8 +14,9 @@ sys.path.append(parent_dir)
 from teleop.open_television.tv_wrapper import TeleVisionWrapper
 from teleop.robot_control.robot_arm import G1_29_ArmController, G1_23_ArmController, H1_2_ArmController, H1_ArmController
 from teleop.robot_control.robot_arm_ik import G1_29_ArmIK, G1_23_ArmIK, H1_2_ArmIK, H1_ArmIK
-from teleop.robot_control.robot_hand_unitree import Dex3_1_Controller, Gripper_Controller
-from teleop.robot_control.robot_hand_inspire import Inspire_Controller
+# from teleop.robot_control.robot_hand_unitree import Dex3_1_Controller, Gripper_Controller
+# from teleop.robot_control.robot_hand_inspire import Inspire_Controller
+from teleop.robot_control.robot_hand_inspire_newgen import InspireControllerNewGen
 from teleop.image_server.image_client import ImageClient
 from teleop.utils.episode_writer import EpisodeWriter
 
@@ -30,7 +31,7 @@ if __name__ == '__main__':
     parser.set_defaults(record = False)
 
     parser.add_argument('--arm', type=str, choices=['G1_29', 'G1_23', 'H1_2', 'H1'], default='G1_29', help='Select arm controller')
-    parser.add_argument('--hand', type=str, choices=['dex3', 'gripper', 'inspire1'], help='Select hand controller')
+    parser.add_argument('--hand', type=str, choices=['inspire1_newgen'], help='Select hand controller')
 
     args = parser.parse_args()
     print(f"args:{args}\n")
@@ -90,28 +91,35 @@ if __name__ == '__main__':
         arm_ctrl = H1_ArmController()
         arm_ik = H1_ArmIK()
 
-    # hand
-    if args.hand == "dex3":
-        left_hand_array = Array('d', 75, lock = True)         # [input]
-        right_hand_array = Array('d', 75, lock = True)        # [input]
-        dual_hand_data_lock = Lock()
-        dual_hand_state_array = Array('d', 14, lock = False)  # [output] current left, right hand state(14) data.
-        dual_hand_action_array = Array('d', 14, lock = False) # [output] current left, right hand action(14) data.
-        hand_ctrl = Dex3_1_Controller(left_hand_array, right_hand_array, dual_hand_data_lock, dual_hand_state_array, dual_hand_action_array)
-    elif args.hand == "gripper":
-        left_hand_array = Array('d', 75, lock=True)
-        right_hand_array = Array('d', 75, lock=True)
-        dual_gripper_data_lock = Lock()
-        dual_gripper_state_array = Array('d', 2, lock=False)   # current left, right gripper state(2) data.
-        dual_gripper_action_array = Array('d', 2, lock=False)  # current left, right gripper action(2) data.
-        gripper_ctrl = Gripper_Controller(left_hand_array, right_hand_array, dual_gripper_data_lock, dual_gripper_state_array, dual_gripper_action_array)
-    elif args.hand == "inspire1":
+    # have to comment out because of dependency conflict with inspire_sdk (used for new hands)
+    # if args.hand == "dex3":
+    #     left_hand_array = Array('d', 75, lock = True)         # [input]
+    #     right_hand_array = Array('d', 75, lock = True)        # [input]
+    #     dual_hand_data_lock = Lock()
+    #     dual_hand_state_array = Array('d', 14, lock = False)  # [output] current left, right hand state(14) data.
+    #     dual_hand_action_array = Array('d', 14, lock = False) # [output] current left, right hand action(14) data.
+    #     hand_ctrl = Dex3_1_Controller(left_hand_array, right_hand_array, dual_hand_data_lock, dual_hand_state_array, dual_hand_action_array)
+    # elif args.hand == "gripper":
+    #     left_hand_array = Array('d', 75, lock=True)
+    #     right_hand_array = Array('d', 75, lock=True)
+    #     dual_gripper_data_lock = Lock()
+    #     dual_gripper_state_array = Array('d', 2, lock=False)   # current left, right gripper state(2) data.
+    #     dual_gripper_action_array = Array('d', 2, lock=False)  # current left, right gripper action(2) data.
+    #     gripper_ctrl = Gripper_Controller(left_hand_array, right_hand_array, dual_gripper_data_lock, dual_gripper_state_array, dual_gripper_action_array)
+    # elif args.hand == "inspire1":
+    #     left_hand_array = Array('d', 75, lock = True)          # [input]
+    #     right_hand_array = Array('d', 75, lock = True)         # [input]
+    #     dual_hand_data_lock = Lock()
+    #     dual_hand_state_array = Array('d', 12, lock = False)   # [output] current left, right hand state(12) data.
+    #     dual_hand_action_array = Array('d', 12, lock = False)  # [output] current left, right hand action(12) data.
+    #     hand_ctrl = Inspire_Controller(left_hand_array, right_hand_array, dual_hand_data_lock, dual_hand_state_array, dual_hand_action_array)
+    if args.hand == "inspire1_newgen":
         left_hand_array = Array('d', 75, lock = True)          # [input]
         right_hand_array = Array('d', 75, lock = True)         # [input]
         dual_hand_data_lock = Lock()
         dual_hand_state_array = Array('d', 12, lock = False)   # [output] current left, right hand state(12) data.
         dual_hand_action_array = Array('d', 12, lock = False)  # [output] current left, right hand action(12) data.
-        hand_ctrl = Inspire_Controller(left_hand_array, right_hand_array, dual_hand_data_lock, dual_hand_state_array, dual_hand_action_array)
+        hand_ctrl = InspireControllerNewGen(left_hand_array, right_hand_array, dual_hand_data_lock, dual_hand_state_array, dual_hand_action_array)
     else:
         pass
     
@@ -161,29 +169,24 @@ if __name__ == '__main__':
                 # record data
                 if args.record:
                     # dex hand or gripper
-                    if args.hand == "dex3":
-                        with dual_hand_data_lock:
-                            left_hand_state = dual_hand_state_array[:7]
-                            right_hand_state = dual_hand_state_array[-7:]
-                            left_hand_action = dual_hand_action_array[:7]
-                            right_hand_action = dual_hand_action_array[-7:]
-                    elif args.hand == "gripper":
-                        with dual_gripper_data_lock:
-                            left_hand_state = [dual_gripper_state_array[1]]
-                            right_hand_state = [dual_gripper_state_array[0]]
-                            left_hand_action = [dual_gripper_action_array[1]]
-                            right_hand_action = [dual_gripper_action_array[0]]
-                    elif args.hand == "inspire1":
-                        left_hand_state = []
-                        right_hand_state = []
-                        left_hand_action = []
-                        right_hand_action = []
-                        pass
+                    # if args.hand == "dex3":
+                    #     with dual_hand_data_lock:
+                    #         left_hand_state = dual_hand_state_array[:7]
+                    #         right_hand_state = dual_hand_state_array[-7:]
+                    #         left_hand_action = dual_hand_action_array[:7]
+                    #         right_hand_action = dual_hand_action_array[-7:]
+                    # elif args.hand == "gripper":
+                    #     with dual_gripper_data_lock:
+                    #         left_hand_state = [dual_gripper_state_array[1]]
+                    #         right_hand_state = [dual_gripper_state_array[0]]
+                    #         left_hand_action = [dual_gripper_action_array[1]]
+                    #         right_hand_action = [dual_gripper_action_array[0]]
+                    if args.hand == "inspire1" or args.hand == "inspire1_newgen":
+                        left_hand_state = dual_hand_state_array[:6]
+                        right_hand_state = dual_hand_state_array[-6:]
+                        left_hand_action = dual_hand_action_array[:6]
+                        right_hand_action = dual_hand_action_array[-6:]
                     else:
-                        left_hand_state = []
-                        right_hand_state = []
-                        left_hand_action = []
-                        right_hand_action = []
                         pass
                     # head image
                     current_tv_image = tv_img_array.copy()
@@ -225,12 +228,12 @@ if __name__ == '__main__':
                                 "torque": [],                         
                             },                        
                             "left_hand": {                                                                    
-                                "qpos":   [],           
+                                "qpos":   left_hand_state,           
                                 "qvel":   [],                           
                                 "torque": [],                          
                             }, 
                             "right_hand": {                                                                    
-                                "qpos":   [],       
+                                "qpos":   right_hand_state,       
                                 "qvel":   [],                           
                                 "torque": [],  
                             }, 
